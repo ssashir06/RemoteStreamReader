cefe53c13c05645a0ea2b1def9f29cd6de96c90f
Hiroshi Takeda <takejaponpon+git@gmail.com>
2015-07-24 01:00:04 +0900

    add Test project. add selenium test. (just open IE)

---
+++ RemoteStreamReader/Remote7zReaderWeb.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace Remote7zReaderWeb.Test
+{
+    [TestClass]
+    public class UnitTest_Selenium
+    {
+        [TestMethod]
+        public void TestMethod_IE_Opening()
+        {
+            using (IWebDriver webdriver = new OpenQA.Selenium.IE.InternetExplorerDriver(@"drivers"))
+            {
+                webdriver.Navigate().GoToUrl("about:blank");
+                Assert.AreEqual("about:blank", webdriver.Url);
+                webdriver.Quit();
+            }
+        }
+    }
+}
